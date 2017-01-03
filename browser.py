9929d42703453edc004b44d81fe56293c125098a
Roger <416251221@qq.com>
2017-01-03 18:50:07 +0800

    Change some files

--- browser.py
+++ browser.py
@@ -1,4 +1,4 @@
-# v1.1
+# v1.2
 # created
 #   by Roger
 # in 2017.1.3
@@ -11,23 +11,50 @@ from PyQt5.QtWebKitWidgets import *
 import sys
 
 class MainWindow(QMainWindow):
+    # noinspection PyUnresolvedReferences
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         # 设置窗口标题
         self.setWindowTitle('My Browser')
         # 设置窗口图标
         self.setWindowIcon(QIcon('icons/penguin.png'))
-        #设置窗口大小900*600
+        # 设置窗口大小900*600
         self.resize(900, 600)
         self.show()
 
         # 设置浏览器
         self.browser = QWebView()
-        url = 'https://www.baidu.com/'
+        url = 'http://blog.csdn.net/roger_lzh'
         # 指定打开界面的 URL
         self.browser.setUrl(QUrl(url))
         # 添加浏览器到窗口中
         self.setCentralWidget(self.browser)
+
+
+        ###使用QToolBar创建导航栏，并使用QAction创建按钮
+        # 添加导航栏
+        navigation_bar = QToolBar('Navigation')
+        # 设定图标的大小
+        navigation_bar.setIconSize(QSize(16, 16))
+        self.addToolBar(navigation_bar)
+
+        #QAction类提供了抽象的用户界面action，这些action可以被放置在窗口部件中
+        # 添加前进、后退、停止加载和刷新的按钮
+        back_button = QAction(QIcon('icons/back.png'), 'Back', self)
+        next_button = QAction(QIcon('icons/next.png'), 'Forward', self)
+        stop_button = QAction(QIcon('icons/cross.png'), 'stop', self)
+        reload_button = QAction(QIcon('icons/renew.png'), 'reload', self)
+
+        back_button.triggered.connect(self.browser.back)
+        next_button.triggered.connect(self.browser.forward)
+        stop_button.triggered.connect(self.browser.stop)
+        reload_button.triggered.connect(self.browser.reload)
+
+        # 将按钮添加到导航栏上
+        navigation_bar.addAction(back_button)
+        navigation_bar.addAction(next_button)
+        navigation_bar.addAction(stop_button)
+        navigation_bar.addAction(reload_button)
 
 
 # 创建应用
